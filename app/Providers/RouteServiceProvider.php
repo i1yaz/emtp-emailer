4626418b2797b9f898d44db84638dca156a9d4c6
Ilyas <danish.ilyas786@gmail.com>
2022-04-08 16:20:39 +0500

    setup infyom generator

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -46,6 +47,11 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+            if (!App::environment(['production'])) {
+                Route::middleware('web')
+                    ->namespace($this->namespace)
+                    ->group(base_path('routes/infoyomlabs.php'));
+            }
         });
     }
 
---
+++ routes/infoyomlabs.php
@@ -0,0 +1,18 @@
+<?php
+
+use InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController;
+
+Route::get('generator_builder',)->name('io_generator_builder');
+
+Route::get('field_template', [GeneratorBuilderController::class, 'fieldTemplate'])->name('io_field_template');
+
+Route::get('relation_field_template', [GeneratorBuilderController::class, 'relationFieldTemplate'])->name('io_relation_field_template');
+
+Route::post('generator_builder/generate',  [GeneratorBuilderController::class, 'generate'])->name('io_generator_builder_generate');
+
+Route::post('generator_builder/rollback', [GeneratorBuilderController::class, 'rollback'])->name('io_generator_builder_rollback');
+
+Route::post(
+    'generator_builder/generate-from-file',
+    [GeneratorBuilderController::class, 'generateFromFile']
+)->name('io_generator_builder_generate_from_file');
