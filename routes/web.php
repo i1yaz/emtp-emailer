92e963898d3697774ed0e12fb98b5d36250628cb
Ilyas <danish.ilyas786@gmail.com>
2022-04-15 14:45:47 +0500

    fisable registration

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::group(['middleware' => ['auth']], function () {
 
